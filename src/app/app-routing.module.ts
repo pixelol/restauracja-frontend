ced63a9086637b3498bba76ea017f1ba4c24a5b3
Kamil Mocko <kamil5294@onet.pl>
2020-09-13 11:58:07 +0200

    glówny panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
+import { LogoutPageComponent } from './pages/logout-page/logout-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
+import { FoodPageComponent } from './pages/food-page/food-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'food', component: FoodPageComponent },
+  { path: 'shopping-cart', component: ShoppingCartPageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'logout', component: LogoutPageComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  LoginPageComponent,
+  HomePageComponent,
+  FoodPageComponent,
+  ShoppingCartPageComponent,
+  AdminPageComponent,
+  LogoutPageComponent
+];
