7c52101501a1d4160327dc14aac0e345a7c85b4f
Kamil Mocko <kamil5294@onet.pl>
2020-09-15 15:26:39 +0200

    update

--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -11,6 +11,15 @@ export class AdminPageComponent implements OnInit {
 
   foods: Array<Food>;
 
+  showUpdatePannel: boolean;
+
+  updateTypeInput: string;
+  updateNameInput: string;
+  updateLeftSidePriceInput: number;
+  updateRightSidePriceInput: number;
+  updateId: number;
+  updateStatus: string;
+
   constructor(private foodService: FoodService) { }
 
   ngOnInit(): void {
@@ -35,6 +44,60 @@ export class AdminPageComponent implements OnInit {
     this.foodService.HttpGetFoodByType(type).subscribe(e => {
       this.foods = e;
     });
+  }
+
+  setFoodType(type: string): void {
+    this.updateTypeInput = type;
+  }
+
+  updateFood(food: Food): void {
+    const priceSplitted = food.price.split('.', 2);
+
+    this.updateTypeInput = food.type;
+    this.updateNameInput = food.name;
+    this.updateLeftSidePriceInput = +priceSplitted[0];
+    this.updateRightSidePriceInput = +priceSplitted[1];
+    this.updateId = food.id;
+    this.updateStatus = food.status;
+
+    this.showUpdatePannel = true;
+  }
+
+  back(): void {
+    this.updateTypeInput = null;
+    this.updateNameInput = null;
+    this.updateLeftSidePriceInput = null;
+    this.updateRightSidePriceInput = null;
+    this.updateId = null;
+    this.updateStatus = null;
+
+    this.showUpdatePannel = false;
+  }
+
+  save(): void {
+    let connectPrice: string;
+    if (this.updateRightSidePriceInput < 10) {
+      connectPrice = this.updateLeftSidePriceInput + '.' + '0' + this.updateRightSidePriceInput;
+    } else {
+      connectPrice = this.updateLeftSidePriceInput + '.' + this.updateRightSidePriceInput;
+    }
+
+    const updatingFood: Food = {
+      id: this.updateId,
+      name: this.updateNameInput,
+      price: connectPrice,
+      status: this.updateStatus,
+      type: this.updateTypeInput
+    };
+
+    this.foodService.HttpUpdateFood(updatingFood).subscribe(e => {
+      console.log(e);
+    });
+    this.back();
+  }
+
+  deleteFood(food: Food): void {
+    this.foodService.HttpDeleteFood(food).subscribe();
   }
 
 }
--- src/app/services/food-service/food.service.ts
+++ src/app/services/food-service/food.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { Food } from 'src/app/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,18 @@ export class FoodService {
   HttpGetFoodByType(type: string): any {
     const param = new HttpParams().set('type', type + '');
     return this.http.get<Array<User>>('http://localhost:8080/api/food/type', { params: param });
+  }
+
+  HttpAddFood(food: Food): Observable<any> {
+    return this.http.post<Food>('http://localhost:8080/api/food', food);
+  }
+
+  HttpUpdateFood(food: Food): Observable<any> {
+    return this.http.put<Food>('http://localhost:8080/api/food', food);
+  }
+
+  HttpDeleteFood(food: Food): Observable<any> {
+    const param = new HttpParams().set('id', food.id + '');
+    return this.http.delete<Food>('http://localhost:8080/api/food', { params: param });
   }
 }
